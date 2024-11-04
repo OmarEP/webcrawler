230b47aeb0dd29ac9c9ea1148268b7201ec06e7d
OmarEP <omrspz@gmail.com>
2024-11-04 16:16:40 -0500

    add getHTML

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	html := `
-<html>
-	<body>
-		<a href="/path/one">
-			<span>Boot.dev</span>
-		</a>
-		<a href="https://other.com/path/one">
-			<span>Boot.dev</span>
-		</a>
-	</body>
-</html>
-`
-	url := "https://blog.boot.dev"
 
-	fmt.Println(getURLsFromHTML(html, url))
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	rawBaseURL := os.Args[1]
+
+	fmt.Printf("starting crawl of: %s\n", rawBaseURL)
+
+	htmlBody, err := getHTML(rawBaseURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Print(htmlBody)
 }
